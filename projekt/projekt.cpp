49497306a23d4cd5036d4b4a83e9e2f0b1f476a7
DanielMikolas <mikolas@stud.uniza.sk>
2025-03-09 17:36:02 +0100

    Dankove upravy

--- projekt/projekt.cpp
+++ projekt/projekt.cpp
@@ -1,20 +1,20 @@
-// projekt.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "Loader.h"
 
-int main()
-{
-    
+int main() {
+    Loader loader;
+    std::string filepath = "/Users/danielpc/Downloads/Iris.csv"; // Nahraď názvom svojho súboru
+    std::vector<Set> sets = loader.load(filepath);
+
+    // Výpis výsledkov s klasickými for cyklami
+    for (size_t i = 0; i < sets.size(); i++) {
+        Set set = sets[i];
+
+        for (size_t j = 0; j < set.getAttributes().size(); j++) {
+            Point attr = set.getAttributes()[j];
+            std::cout << "Hodnota: " << attr.value << ", Trieda: " << attr.className << std::endl;
+        }
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
